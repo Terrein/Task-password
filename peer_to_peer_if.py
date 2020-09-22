3dc954690cc5080a041677aa07047f7d4d94b080
Terrein <Terrein22@yandex.ru>
2020-09-23 00:32:47 +0300

    relize 3

--- peer_to_peer_if.py
+++ peer_to_peer_if.py
@@ -7,13 +7,13 @@ users = [{'name': 'Иван', 'age': 35},
 tourists = [{'user': users[0], 'city': cities[0]},
             {'user': users[1], 'city': cities[1]},
             {'user': users[2], 'city': cities[2]}]
- 
- #Введенная переменная приводится к виду членов словаря cities
+
+# Введенная переменная приводится к виду членов словаря cities
 city = input('Введите город: ').capitalize()
 
-#Проверяется содержится ли горд в списке
+# Проверяется содержится ли горд в списке
 if city in cities:
-    index_num = cities.index(city) #определяю индеск найденого члена списка
+    index_num = cities.index(city)  # определяю индеск найденого члена списка
     print(
         f"Турист {tourists[index_num]['user']['name']} возраст {tourists[index_num]['user']['age']}. Посетил город {tourists[index_num]['city']}")
 else:
