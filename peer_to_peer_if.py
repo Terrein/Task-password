c1c5637a1aec7902e5cb427b9591326549d78df4
Terrein <Terrein22@yandex.ru>
2020-09-24 00:28:43 +0300

    relize 3

--- peer_to_peer_if.py
+++ peer_to_peer_if.py
@@ -1,20 +1,31 @@
-cities = ['Москва', 'Париж', 'Лондон']
+cities = ['Орел','Москва', 'Париж', 'Лондон']
 
 users = [{'name': 'Иван', 'age': 35},
          {'name': 'Мария', 'age': 22},
          {'name': 'Соня', 'age': 20}]
 
-tourists = [{'user': users[0], 'city': cities[0]},
-            {'user': users[1], 'city': cities[1]},
+tourists = [{'user': users[1], 'city': cities[1]},
+            {'user': users[0], 'city': cities[0]},
             {'user': users[2], 'city': cities[2]}]
 
 # Введенная переменная приводится к виду членов словаря cities
-city = input('Введите город: ').capitalize()
+city = input('Введите город: ').strip().capitalize()
 
-# Проверяется содержится ли горд в списке
 if city in cities:
+    for tourist in tourists:
+        if tourist['city'] == city:
+            print(f"Турист {tourist['user']['name']} возраст {tourist['user']['age']}. Посетил город {tourist['city']}")
+        else:
+            print(f"Турист {tourist['user']['name']} не посещал {city}")           
+else:
+    print("Город не найден")     
+
+
+  # Проверяется содержится ли горд в списке
+"""if city in cities:
     index_num = cities.index(city)  # определяю индеск найденого члена списка
     print(
         f"Турист {tourists[index_num]['user']['name']} возраст {tourists[index_num]['user']['age']}. Посетил город {tourists[index_num]['city']}")
 else:
-    print("Город не найден")
+    print("Город не найден") 
+  """
