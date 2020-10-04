02172a98daf874b4dffcdbfb0a6a5722349d6825
Terrein <Terrein22@yandex.ru>
2020-10-04 19:28:41 +0300

    Two options of tasks

--- peer_to_peer_get_absolute_url.py
+++ peer_to_peer_get_absolute_url.py
@@ -22,7 +22,7 @@ print(get_absolute_url('www.da-office.ru', 'store', 'zashitnii-kovriki-pod-kresl
 #Тестовый вывод
 print(get_absolute_url('yandex.ru','news', utm_source='main_stripe_big'))
 
-""" Сделал в двух вариантах т.к. считаю, что с \ перед ? правильнее. 
+""" Сделал в двух вариантах т.к. считаю, что с / перед ? правильнее. 
 В некоторых случаях не влияет на вывод, но можете затестировать ввывод например с такой ссылкой: 
 https://www.da-office.ru/store/zashitnii-kovriki-pod-kreslo/bsl-100-x-120-sm-1-2-mm-matovyy-pesok-polipropilen/?utm_source=YM&utm_medium=cpc&utm_content=bsl-100-x-120-sm-1-2-mm-matovyy-pesok-polipropilen&utm_campaign=zashitnii-kovriki-pod-kreslo&r1=&ymclid=16012405962867952112500007
 в ней принципиален / перед ? """
