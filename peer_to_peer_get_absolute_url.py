9950ea8fef94d030ea3f4c395b69b255bbbbcb0d
Terrein <Terrein22@yandex.ru>
2020-10-04 19:19:49 +0300

    Two options of tasks

--- peer_to_peer_get_absolute_url.py
+++ peer_to_peer_get_absolute_url.py
@@ -15,10 +15,25 @@ def get_absolute_url(url, *args, **kwargs):
 
 # https://www.da-office.ru/store/zashitnii-kovriki-pod-kreslo/bsl-100-x-120-sm-1-2-mm-matovyy-pesok-polipropilen/?utm_source=YM&utm_medium=cpc&utm_content=bsl-100-x-120-sm-1-2-mm-matovyy-pesok-polipropilen&utm_campaign=zashitnii-kovriki-pod-kreslo&r1=&ymclid=16012405962867952112500007
 #Тестовый вывод
-print(get_absolute_url('www.da-office.ru', 'store', 'zashitnii-kovriki-pod-kreslo', 'bsl-100-x-120-sm-1-2-mm-matovyy-pesok-polipropilen', utm_source='YM',
+""" print(get_absolute_url('www.da-office.ru', 'store', 'zashitnii-kovriki-pod-kreslo', 'bsl-100-x-120-sm-1-2-mm-matovyy-pesok-polipropilen', utm_source='YM',
                        utm_medium='cpc', utm_content='bsl-100-x-120-sm-1-2-mm-matovyy-pesok-polipropilen', 
-                       utm_campaign='zashitnii-kovriki-pod-kreslo', r1='', ymclid=16012405962867952112500007))
+                       utm_campaign='zashitnii-kovriki-pod-kreslo', r1='', ymclid=16012405962867952112500007)) """
 #Тестовый вывод
-print(get_absolute_url('yandex.ru','news', utm_source='main_stripe_big'))
+#print(get_absolute_url('yandex.ru','news', utm_source='main_stripe_big'))
 
-
+def get_absolute_url_2(url, *args, **kwargs):
+    """ Сборка url-адресса по входящим параметрам. 
+    :param ulr: обязательный аргумент.
+    :param *args: позиционный(е) аргумент(ы). Возможен ввод любого кол-ва аргументов
+    :param **kwargs: именованный(е) аргумент(ы). Возможен ввод любого кол-ва аргументов
+    """  
+    str_url = ""
+    for el in args:
+        str_url += el.strip() + "/"
+    str_url = str_url[:-1] + "?"
+    for k, v in kwargs.items():
+        str_url += ((k.strip()) + "=" + str(v).strip() + "&")
+    str_url = str_url[:-1]
+    return(url + "/" + str_url)
+    
+print(get_absolute_url_2('yandex.ru','news', utm_source='main_stripe_big'))
